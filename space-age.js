763a61babf9379ae261a676c80b5a1452e047c02
davidagitlen <davidagitlen@gmail.com>
2019-12-17 22:08:04 -0700

    Refactor solutino to pass all tests with const/object[key]

--- space-age.js
+++ space-age.js
@@ -4,25 +4,17 @@
 //31,557,600
 
 export const age = (planet, seconds) => {
-  let earthYears = seconds/31557600
-  switch (planet) {
-    case 'mercury':
-      return parseFloat((earthYears/.2408467).toFixed(2));
-    case 'venus':
-      return parseFloat((earthYears/0.61519726).toFixed(2));
-    case 'earth' : 
-      return parseFloat(earthYears.toFixed(2));
-    case 'mars' :
-      return parseFloat((earthYears/1.8808158).toFixed(2));
-    case 'jupiter' :
-      return parseFloat((earthYears/11.862615).toFixed(2));
-    case 'saturn' :
-      return parseFloat((earthYears/29.447498).toFixed(2));
-    case 'uranus' :
-      return parseFloat((earthYears/84.016846).toFixed(2));
-    case 'neptune' :
-      return parseFloat((earthYears/164.79132).toFixed(2));
-    default :
-      return
+  const earthYear = 31557600;
+  const planets = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }
+
+  return Number(((seconds/earthYear)/planets[planet]).toFixed(2))
 };
